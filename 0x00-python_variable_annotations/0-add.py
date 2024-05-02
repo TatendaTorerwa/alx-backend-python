67eba4d30e5a906933e66964a64b3e96e38400f1
TatendaTorerwa <ttorerwa01@gmail.com>
2024-05-02 18:04:57 +0000

    0. Basic annotations - add.

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Basic annotations - add."""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two floats together.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+
+    return a + b
