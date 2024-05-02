59295640cab04ba23ae86619e39bd9df61fc2eb1
TatendaTorerwa <ttorerwa01@gmail.com>
2024-05-02 18:11:26 +0000

    1. Basic annotations - concat.

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Basic annotations - add."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string of str1 and str2.
+    """
+
+    return str1 + str2
