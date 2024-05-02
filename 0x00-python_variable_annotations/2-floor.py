8fe51da7545707297eff0cba3578a1f37955c536
TatendaTorerwa <ttorerwa01@gmail.com>
2024-05-02 18:16:25 +0000

    2. Basic annotations - floor.'

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a float.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        int: The floor of the input float.
+    """
+
+    return int(n)
