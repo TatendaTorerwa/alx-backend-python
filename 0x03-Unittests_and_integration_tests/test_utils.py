0e8a4b6194166992d41cdd30d98b8298ae751439
TatendaTorerwa <ttorerwa01@gmail.com>
2024-05-27 17:20:08 +0000

    3. Parameterize and patch.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Testing the utils module.
+"""A module for testing the utils module.
 """
 import unittest
 from typing import Dict, Tuple, Union
@@ -14,7 +14,7 @@ from utils import (
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Tests the `access_nested_map` function using parameterized"""
+    """Tests the `access_nested_map` function."""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -50,45 +50,35 @@ class TestGetJson(unittest.TestCase):
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
-    @patch('utils.requests.get')
     def test_get_json(
             self,
             test_url: str,
             test_payload: Dict,
-            mock_get
             ) -> None:
-
-        """Create a Mock response object with a json method
-        that returns test_payload."""
-        mock_response = Mock()
-        mock_response.json.return_value = test_payload
-        mock_get.return_value = mock_response
-
-        """Call the function with the test URL."""
-        result = get_json(test_url)
-
-        """Assert the get method was called once with the test URL."""
-        mock_get.assert_called_once_with(test_url)
-
-        """Assert the function returned the expected payload."""
-        self.assertEqual(result, test_payload)
+        """Tests `get_json`'s output."""
+        attrs = {'json.return_value': test_payload}
+        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            req_get.assert_called_once_with(test_url)
 
 
-class TestClass:
-    def test_memoization(self):
+class TestMemoize(unittest.TestCase):
+    """Tests the `memoize` function."""
+    def test_memoize(self) -> None:
+        """Tests `memoize`'s output."""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
         with patch.object(
                 TestClass,
                 "a_method",
-                return_value=42,
+                return_value=lambda: 42,
                 ) as memo_fxn:
             test_class = TestClass()
             self.assertEqual(test_class.a_property(), 42)
             self.assertEqual(test_class.a_property(), 42)
             memo_fxn.assert_called_once()
-
-    def a_method(self):
-        return 42
-
-    @memoize
-    def a_property(self):
-        return self.a_method()
