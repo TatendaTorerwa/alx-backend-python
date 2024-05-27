8fbb6f22cee43dfb77b8007fc9c2cfd5d1039bc2
TatendaTorerwa <ttorerwa01@gmail.com>
2024-05-27 16:54:32 +0000

    2. Mock HTTP calls.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -49,6 +49,7 @@ class TestGetJson(unittest.TestCase):
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
+    @patch('utils.requests.get')
     def test_get_json(
             self,
             test_url: str,
